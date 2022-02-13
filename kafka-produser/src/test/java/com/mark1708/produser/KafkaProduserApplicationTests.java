de28a9df6e42f47f6f9afeefb570cfc16bfbaa2d
Mark <m.gurianov@mail.ru>
2022-02-13 16:29:47 +0300

    add produser

---
+++ kafka-produser/src/test/java/com/mark1708/produser/KafkaProduserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark1708.produser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProduserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
